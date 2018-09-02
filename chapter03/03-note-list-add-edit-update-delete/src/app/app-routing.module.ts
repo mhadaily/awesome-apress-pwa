2a54873ddac03cbe67d61eb771f1ff0ebef455e4
Majid Hajian <mhadaily@gmail.com>
2018-09-02 16:17:10 +0200

    cleanup chapter3

--- chapter03/03-note-list-add-edit-update-delete/src/app/app-routing.module.ts
+++ chapter03/03-note-list-add-edit-update-delete/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { AuthGuard } from "./modules/core/auth-guard";
+import { AuthGuard } from "./modules/core/auth.guard";
 
 const routes: Routes = [
   {
